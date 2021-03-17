d19ea59d870b834e778bc030fbf5df7bd29b4c27
Kenneth <kenm.riv@gmail.com>
2021-03-17 13:36:31 -0400

    Added login and signup to the app

--- src/main.js
+++ src/main.js
@@ -5,6 +5,23 @@ import './plugins/bootstrap-vue'
 import App from './App.vue'
 import router from './router'
 import store from './store'
+import firebase from 'firebase';
+
+const configOptions = {
+    apiKey: "AIzaSyD46PWxKYRjz-_upmXUzrTMKXK7x4u3phs",
+    authDomain: "imadiq-857ee.firebaseapp.com",
+    projectId: "imadiq-857ee",
+    storageBucket: "imadiq-857ee.appspot.com",
+    messagingSenderId: "899851400756",
+    appId: "1:899851400756:web:5af184eca792ca72e87c61",
+    measurementId: "G-V4GQ0GY67S"
+};
+
+firebase.initializeApp(configOptions);
+
+firebase.auth().onAuthStateChanged(user => {
+  store.dispatch("fetchUser", user);
+});
 
 Vue.config.productionTip = false
 
